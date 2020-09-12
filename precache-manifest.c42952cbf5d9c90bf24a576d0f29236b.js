800e8f8aaa8524247127d473a690795132450568
arnavb <arnavborborah11@gmail.com>
2020-09-12 17:39:08 +0000

    Release: "Fix author name"

---
+++ precache-manifest.c42952cbf5d9c90bf24a576d0f29236b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd65a8614357313be6158bbc041cbf5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8546a60e9c0ce6c8813c",
+    "url": "/static/css/main.40095d4d.chunk.css"
+  },
+  {
+    "revision": "6c47d6260da1280a1cc7",
+    "url": "/static/js/2.d99319c6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d99319c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8546a60e9c0ce6c8813c",
+    "url": "/static/js/main.d9d24a49.chunk.js"
+  },
+  {
+    "revision": "c5c5d11a5123bae317ed",
+    "url": "/static/js/runtime-main.9935317f.js"
+  }
+]);
